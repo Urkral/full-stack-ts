d990029f837e653e4a1854ff9d8db1554075620c
Urkral <esteban.dutrannois@outlook.com>
2022-09-12 17:33:44 +0200

    Cour TS

---
+++ server/src/resolvers.ts
@@ -0,0 +1,22 @@
+import Query from "./resolvers/Query"
+import User from "./resolvers/User"
+import Tweet from "./resolvers/Tweet"
+import Mutation from "./resolvers/Mutation"
+import { Resolvers } from "./resolvers-types.generated"
+import Db, { DbTweet, DbUser } from "./db"
+
+export interface TwitterResolverContext {
+  db: Db,
+  dbTweetCache:Record<string, DbTweet>,
+  dbUserCache:Record<string, DbUser>,
+  dbTweetToFavoriteCountMap:Record<string, number>,
+}
+
+const resolvers: Resolvers<TwitterResolverContext> = {
+  Query,
+  User,
+  Mutation,
+  Tweet
+}
+
+export default resolvers
